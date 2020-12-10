da036b431b0da5432dd321f4383f0ce3a576872f
Joey Brown <gjoey.brown@gmail.com>
2020-12-10 10:59:44 -0800

    added virtual part to schema

--- models/workout.js
+++ models/workout.js
@@ -1,5 +1,6 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
+const opts = { toJSON: { virtuals: true } };
 
 const workoutSchema = new Schema({
     day: {
@@ -8,7 +9,7 @@ const workoutSchema = new Schema({
     },
     exercises: [
         {
-            type:{
+            type: {
                 type: String,
                 trim: true
             },
@@ -33,6 +34,17 @@ const workoutSchema = new Schema({
             }
         }
     ]
+}, opts);
+
+//make total duration function (this will be a column within the model that adds the duration of workouts and then puts that total to the
+//front end. Check Thurs activities for custom models reference.)
+
+workoutSchema.virtual('totalDuration').get(function () {
+    let result = 0;
+    this.exercises.forEach(element => {
+        result += element.duration;
+    });
+    return result;
 });
 
 const Workout = mongoose.model("Workout", workoutSchema);
