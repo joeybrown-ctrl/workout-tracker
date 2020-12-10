4b68bebe029817c507af34ed773ea0adaa37dcb3
Joey Brown <gjoey.brown@gmail.com>
2020-12-10 12:13:19 -0800

    deployed to heroku, connected to atlasDB

--- server.js
+++ server.js
@@ -17,7 +17,15 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", { useNewUrlParser: true });
+mongoose.connect(
+    process.env.MONGODB_URI || 'mongodb://localhost/workout-tracker',
+    {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+        useCreateIndex: true,
+        useFindAndModify: false
+    }
+);
 
 app.get("/", (req, res) => {
     res.sendFile(path.join(__dirname, "./public/index.html"));
